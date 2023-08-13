46c55ac35f624d79df5578ebfcb430ec77b09b8a
Iftekhar Bin Aziz <iftekhar932@gmail.com>
2023-08-13 20:26:23 +0600

    new file

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://www.dandrepair.com",
+  "http://dandrepairshop.com",
+];
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+ const cookieParser = require('cookie-parser')
 
 app.use(cors());
+
+app.use(cookieParser()) 50:47
+
 app.use(express.json());
+
 app.use(logger);
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
@@ -23,6 +31,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
